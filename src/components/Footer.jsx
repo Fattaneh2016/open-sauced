13b7247ff8dfdf36706a6f107cd59c77c73d7378
Fattaneh <fatameri@hotmail.com>
2022-11-10 12:51:59 -0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -41,11 +41,6 @@ function Footer() {
             <FaDev className="text-offWhite hover:text-accent" />
           </a>
         </li>
-        <a href="https://www.dev.to/opensauced"
-                 target="_blank" rel="noreferrer"
-                 aria-label="Visit Open Sauced GitHub Organisation">
-                <FaDev className="text-offWhite hover:text-accent" />
-         </a>
       </ul>
     </Container>
   );
